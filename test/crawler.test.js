555d76ee474a617e9ae76b100f19385d7867ffc5
Sam Clement <sam@swhurl.com>
2016-11-30 13:57:25 +0000

    Add basic crawler

--- test/crawler.test.js
+++ test/crawler.test.js
@@ -1,16 +1,17 @@
-const assert = require('assert')
+const test = require('tape')
 const crawler = require('../lib/crawler')
 
-describe('crawler', function() {
-  describe('#constructor()', function() {
-    it(`should throw an error if passed an 'f' character`, function() {
-      assert.throws(() => { crawler('f') }, /Not a valid url/, Error)
-    })
-    it(`should throw an error if passed a '/'`, function() {
-      assert.throws(() => { crawler('/') }, /Not a valid url/, Error)
-    })
-    it(`should not throw an error if passed a valid url: 'http://www.google.com'`, function() {
-      assert.doesNotThrow(() => { crawler('http://www.google.com') }, /Not a valid url/, Error)
+test('crawler', (t) => {
+  t.plan(2)
+  t.throws(function() { crawler('f') }, /Not a valid url/, `should throw an error if passed an 'f' character`)
+  t.throws(function() { crawler('/') }, /Not a valid url/, `should throw an error if passed a '/'`)
+
+  test(`crawl 'https://buildit.digital'`, (tt) => {
+    crawler('https://buildit.digital', (err, results) => {
+      const firstItem = 'https://buildit.digital/'
+      tt.plan(2)
+      tt.equal(Array.isArray(results), true, `result should be an array`)
+      tt.equal(results[0], firstItem, `first item of array should equal '${firstItem}'`)
     })
   })
 })
