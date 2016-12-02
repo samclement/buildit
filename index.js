c26b56b0da54fcc9d53f821b0feaaec594922c4d
Sam Clement <sam@swhurl.com>
2016-12-02 14:32:40 +0000

    Create sitemap.xml

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ crawler('https://buildit.digital', (err, results) => {
   if (err) {
     console.log(err)
   } else {
-    console.log(results)
+    const stream = fs.createWriteStream('./sitemap.xml')
+    stream.write(`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`)
+    results.forEach((url) => { stream.write(`\n  <url><loc>${url}</loc></url>`) })
+    stream.write(`\n</urlset>`)
+    stream.end()
   }
 })
